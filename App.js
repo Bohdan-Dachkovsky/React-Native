1a26b85710dd7b2a756025fbaec2d2b2b1e81ade
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2024-01-07 02:21:10 +0200

    update package no locale cli

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React-Native</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
