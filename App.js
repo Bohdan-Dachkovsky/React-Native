33106d3f005d0225ef5d0fd87e7aed9711a9d200
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2024-01-07 03:41:41 +0200

    Update

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NODE_ENV } from "@env";
+
 if (NODE_ENV === "development") {
   console.log("Hello in development");
 }
