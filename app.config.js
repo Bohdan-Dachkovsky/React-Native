623f23b5e964df6c7abbf1a161fd027a6d291232
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2024-01-15 02:19:44 +0200

    Change background

--- app.config.js
+++ app.config.js
@@ -1,9 +1,9 @@
-module.exports = ({ config }) => {
-  console.log(config.name);
-  return {
-    ...config,
-    extra: {
-      headers: "React-Native",
-    },
-  };
+module.exports = {
+  name: myValue,
+  version: process.env.MY_CUSTOM_PROJECT_VERSION || "1.0.0",
+  // All values in extra will be passed to your app.
+
+  extra: {
+    headers: "Hello",
+  },
 };
