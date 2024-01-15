2929b7459575fcb5b358bbee6ca89a278edebab8
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2024-01-15 03:55:50 +0200

    Add Image

--- App.js
+++ App.js
@@ -10,6 +10,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar />
+      <Image
+        style={styles.image}
+        src="./assets/splash.png"
+        placeholder={blurhash}
+        contentFit="cover"
+        transition={1000}
       <Text>{Constants.expoConfig.extra.headers} React Native</Text>
     </View>
   );
